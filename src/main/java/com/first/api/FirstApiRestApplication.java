ff848a159b31b11b3e6b76ca55e8846dd212b5fc
Lord Cheeng <seanpaulvevo17@gmail.com>
2025-10-26 20:03:07 -0500

    add: release first api

---
+++ src/main/java/com/first/api/FirstApiRestApplication.java
@@ -0,0 +1,20 @@
+package com.first.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstApiRestApplication {
+
+	public static void main(String[] args) {
+
+		try {
+			SpringApplication.run(FirstApiRestApplication.class, args);
+		} catch (Exception e) {
+		
+			System.out.println("Error al iniciar la aplicacion: " + e.getMessage());
+		}
+		
+	}
+
+}
